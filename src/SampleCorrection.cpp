b52b557d932033bc1886707b20703725b614b4ce
Alex Marsev <alex.marsev@gmail.com>
2015-01-24 19:25:16 +0000

    Use DebugOut() in SampleCorrection::ProcessSample()

--- src/SampleCorrection.cpp
+++ src/SampleCorrection.cpp
@@ -54,11 +54,13 @@ namespace SaneAudioRenderer
         {
             // Drop the sample.
             assert(chunk.IsEmpty());
+            DebugOut("SampleCorrection drop", sampleProps.tStart, sampleProps.tStop);
         }
         else if (!m_bitstream && m_freshSegment && sampleProps.tStart < 0)
         {
             // Crop the sample.
             size_t cropFrames = (size_t)TimeToFrames(m_lastSampleEnd - sampleProps.tStart);
+            DebugOut("SampleCorrection crop", cropFrames, "frames", sampleProps.tStart, sampleProps.tStop);
 
             if (cropFrames > 0)
             {
@@ -82,6 +84,7 @@ namespace SaneAudioRenderer
         {
             // Zero-pad the sample.
             size_t padFrames = (size_t)TimeToFrames(sampleProps.tStart - m_lastSampleEnd);
+            DebugOut("SampleCorrection pad", padFrames, "frames", sampleProps.tStart, sampleProps.tStop);
 
             if (padFrames > 0)
             {
