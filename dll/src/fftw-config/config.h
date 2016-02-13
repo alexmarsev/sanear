0f6adfdd0a4d1904740a679feb266a650c96b3a1
Alex Marsev <alex.marsev@gmail.com>
2016-02-13 20:06:31 +0300

    Use FFTW library with rubberband
    Better than default KissFFT and a lot larger.
    
    As far as sanear core branch is concerned, both can be used interchangeably.
    Selection is done by preprocessor definitions during the compilation.

---
+++ dll/src/fftw-config/config.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define FFTW_DOUBLE 1
+#define HAVE_SSE2 1
+
+#define DISABLE_FORTRAN 1
+#define FFTW_ENABLE_ALLOCA 1
+
+#define HAVE_ALLOCA 1
+#define HAVE_ABORT 1
+#define HAVE_UINTPTR_T 1
+
+#define HAVE_INTTYPES_H 1
+#define HAVE_MALLOC_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_TYPES_H 1
+
+#define FFTW_CC "cl"
+#define PACKAGE "fftw"
+#define VERSION "3.3.4"
+#define PACKAGE_VERSION VERSION
